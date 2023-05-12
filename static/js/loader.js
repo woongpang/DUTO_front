4daaf4beb256c4353595e8fd339c54c66cf9c09a
minjeong <ehdro418@naver.com>
2023-05-12 10:03:54 +0900

    init project

---
+++ static/js/loader.js
@@ -0,0 +1,71 @@
+console.log("loader 로드됨")
+
+// navbar.html을 가져옴
+// 로그인 되지 않은 상태에서는 글쓰기가 안 보이고, 로그인 된 상태라면 로그인이 안 보이고 로그아웃 버튼이 생김
+async function injectNavbar() {
+    fetch("../navbar.html").then(response => {
+        return response.text()
+    })
+        .then(data => {
+            document.querySelector("header").innerHTML = data;
+        })
+
+    let navbarHtml = await fetch("../navbar.html")
+    let data = await navbarHtml.text()
+    document.querySelector("header").innerHTML = data;
+
+    const payload = localStorage.getItem("payload")
+    if (payload) {
+        const payload_parse = JSON.parse(payload)
+
+        const intro = document.getElementById("intro")
+        intro.innerText = `${payload_parse.name}님 안녕하세요!`
+
+        let navbarLeft = document.getElementById("navbar-left")
+        let postLi = document.createElement("li")
+        postLi.setAttribute("class", "nav-item")
+
+        let postLink = document.createElement("a")
+        postLink.setAttribute("href", "/posts/post_create.html")
+        postLink.setAttribute("class", "nav-link")
+        postLink.innerHTML = "글쓰기"
+
+        postLi.appendChild(postLink)
+        navbarLeft.appendChild(postLi)
+
+        let navbarRight = document.getElementById("navbar-right")
+        let newLi = document.createElement("li")
+        newLi.setAttribute("class", "nav-item")
+
+        let logoutBtn = document.createElement("button")
+        logoutBtn.setAttribute("class", "nav-link btn")
+        logoutBtn.innerText = "로그아웃"
+        logoutBtn.setAttribute("onclick", "handleLogout()")
+
+        newLi.appendChild(logoutBtn)
+
+        navbarRight.appendChild(newLi)
+
+        let loginButton = document.getElementById("login-button")
+        loginButton.style.display = "none"
+    }
+}
+
+injectNavbar();
+
+// 공부 or 휴식 게시판 클릭 시 html에 있는 카테고리 이름을 가져와서 postCategory 함수를 실행함
+function handleClickCategory(e) {
+    const categoryName = e
+    console.log(categoryName)
+    postCategory(categoryName);
+}
+
+// url에 카테고리 이름을 담기 위한 작업
+function postCategory(category_name) {
+    window.location.href = `${frontend_base_url}/posts/post_list.html?category=${category_name}`
+}
+
+// url에 게시글 pk값을 담기 위한 작업
+function postDetail(post_id) {
+    window.location.href = `${frontend_base_url}/posts/post_detail.html?post_id=${post_id}`
+}
---
+++ static/js/post_list.js
@@ -0,0 +1,50 @@
+console.log("post list js 로드됨")
+
+
+// 카테고리별 게시글 리스트 보기
+window.onload = async function loadPosts() {
+    // url에서 카테고리 이름을 가져옴
+    const urlParams = new URLSearchParams(window.location.search);
+    const categoryName = urlParams.get("category");
+    console.log(categoryName)
+
+    // 카테고리 이름을 매개변수로 백엔드에서 해당 카테고리의 글들을 가져옴
+    posts = await getPosts(categoryName)
+    console.log(posts)
+
+    // 게시글 목록 UI
+    const postCategoryList = document.getElementById("post-category-list")
+
+    posts.forEach(post => {
+        console.log(post)
+        const newCol = document.createElement("div");
+        newCol.setAttribute("class", "col")
+        newCol.setAttribute("onclick", `postDetail(${post.pk})`)
+
+        const newCard = document.createElement("div")
+        newCard.setAttribute("class", "card h-100")
+        newCard.setAttribute("id", `post-${post.pk}`)
+        newCol.appendChild(newCard)
+
+        const postImage = document.createElement("img")
+        postImage.setAttribute("class", "card-img-top")
+
+        if (post.image) {
+            postImage.setAttribute("src", `${backend_base_url}${post.image}`)
+        } else {
+            postImage.setAttribute("src", "https://cdn11.bigcommerce.com/s-1812kprzl2/images/stencil/original/products/426/5082/no-image__12882.1665668288.jpg?c=2")
+        }
+        newCard.appendChild(postImage)
+
+        const newCardBody = document.createElement("div")
+        newCardBody.setAttribute("class", "card-body")
+        newCard.appendChild(newCardBody)
+
+        const newCardTile = document.createElement("h5")
+        newCardTile.setAttribute("class", "card-title")
+        newCardTile.innerText = post.title
+        newCardBody.appendChild(newCardTile)
+
+        postCategoryList.appendChild(newCol)
+    });
+}
