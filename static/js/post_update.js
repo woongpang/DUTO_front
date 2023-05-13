e6daddb15c3960f3f312fd687f94f251020a798e
minjeong <ehdro418@naver.com>
2023-05-14 02:00:34 +0900

    게시글 수정 시 별 색칠되는 부분 코드 수정

--- static/js/post_update.js
+++ static/js/post_update.js
@@ -3,27 +3,22 @@ checkNotLogin();
 window.onload = async function loadUpdatePost() {
     const urlParams = new URLSearchParams(window.location.search);
     const postId = urlParams.get("post_id");
-    console.log(postId)
-
-    const response = await getPost(postId);
-    console.log(response)
+    const exist_post = await getPost(postId);
 
     // 원래 제목 띄우기
     const updateTitle = document.getElementById("update-title")
-    updateTitle.value = response.title
+    updateTitle.value = exist_post.title
 
     // 원래 내용 띄우기
     const updateContent = document.getElementById("update-content")
-    updateContent.value = response.content
+    updateContent.value = exist_post.content
 
     // 원래 이미지 불러오기
     const updateImage = document.getElementById("update-image")
-    updateImage.files[0] = response.image
+    updateImage.files[0] = exist_post.image
 
     //원래 별 띄우기
-    const updateStar = document.getElementById(`rating${response.star}`)
-    const star = updateStar.getAttribute("value")
-    for (let i = 1; i <= star; i++) {
+    for (let i = 1; i <= exist_post.star; i++) {
         document.getElementById(`rating${i}`).checked = true;
     }
 }
