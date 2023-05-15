38549eac28a90c01fa17bc222e374311e9400510
minjeong <ehdro418@naver.com>
2023-05-15 13:16:42 +0900

    비로그인 시 상세게시글 안 보이는 오류 수정 및 각종 주석 삭제

--- static/js/index.js
+++ static/js/index.js
@@ -3,12 +3,5 @@ window.onload = async function loadPosts() {
     posts = await getAllPosts()
     console.log(posts)
     const post_list = document.getElementById("post-list")
-    postList(posts, post_list);   
-
-    // posts.forEach(post=>{
-    //     //원래 별 띄우기
-    // for (let i = 1; i <= post.star; i++) {
-    //     document.getElementById(`rating${i}`).checked = true;
-    // }
-    // })
+    postList(posts, post_list);
 }
--- static/js/profile.js
+++ static/js/profile.js
@@ -1,44 +1,30 @@
-
-
 // html 화면에 사용자 이름을 띄우는 방법. 
 // payload_parse는 payloada만 하면 str형태로 가져오는데 이를 object 형태로 가져오기 위함이다.
 window.onload = async function () {
-    
+
     const payload = localStorage.getItem("payload")
     const payload_parse = JSON.parse(payload)
 
-
     let token = localStorage.getItem("access")
 
-    // function moveDetail(article_id) {
-    //     window.location.href = `${frontend_base_url}/doc/detail.html?article_id=${article_id}`;
-    // }
-    
-    
-
-    const my_respose = await fetch(`${backend_base_url}/users/${payload_parse.user_id}/`,{
-        headers:{
-            "Authorization" : `Bearer ${token}`
+    await fetch(`${backend_base_url}/users/${payload_parse.user_id}/`, {
+        headers: {
+            "Authorization": `Bearer ${token}`
         },
-        method:"GET",
+        method: "GET",
     })
-    
-
-    const my_json_response = await my_respose.json()
-    
 
     let loginButton = document.getElementById("login-button")
     loginButton.style.display = "none"
 
-    const my_posts = await fetch(`${backend_base_url}/users/myposts/`,{
-        headers:{
-            "Authorization" : `Bearer ${token}`
+    const my_posts = await fetch(`${backend_base_url}/users/myposts/`, {
+        headers: {
+            "Authorization": `Bearer ${token}`
         },
-        method:"GET",
+        method: "GET",
     })
 
     const my_json_posts = await my_posts.json()
-    // const my_title = JSON.parse(my_json_posts.title)
 
     console.log(my_json_posts)
 
